da5fc6912c6237a570e67ac0dce519ffa760058b
achraf <achraf.pica@gmail.com>
2020-09-24 21:21:56 +0200

    add hook pipeline

--- build-image.sh
+++ build-image.sh
@@ -6,3 +6,7 @@ JAR_FILE=$(ls build/libs/ | grep "^finance-crypto")
 
 docker build . --build-arg jar=build/libs/$JAR_FILE -t ezzefiohez/finance-crypto
 docker push ezzefiohez/finance-crypto
+
+echo " ######## BUILD CRYPTO DONE ######## "
+
+curl  -X POST http://94.239.109.172:9000/api/webhooks/e2950929-7f5c-402f-aa41-2f6e43b1c67a
