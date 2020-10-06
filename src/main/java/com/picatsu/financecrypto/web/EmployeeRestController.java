a89332267f40dd7d675e41d6128a3f357488d719
achraf <achraf.pica@gmail.com>
2020-10-06 19:24:41 +0200

    retrieve value from token

--- src/main/java/com/picatsu/financecrypto/web/EmployeeRestController.java
+++ src/main/java/com/picatsu/financecrypto/web/EmployeeRestController.java
@@ -2,36 +2,50 @@ package com.picatsu.financecrypto.web;
 
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.picatsu.financecrypto.config.keycloak.JWTDecoder;
 import lombok.extern.slf4j.Slf4j;
-import org.keycloak.RSATokenVerifier;
-import org.keycloak.common.VerificationException;
-import org.keycloak.representations.AccessToken;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.jwt.Jwt;
+import org.springframework.security.jwt.JwtHelper;
+import org.springframework.security.oauth2.provider.token.DefaultAccessTokenConverter;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.security.RolesAllowed;
-import java.security.PublicKey;
+
 
 @RestController
 @RequestMapping("/api/v1/employees")
 @Slf4j
 public class EmployeeRestController {
 
+    @Autowired
+    private JWTDecoder jwtDecoder;
+
     @RequestMapping(value = "/anonymous", method = RequestMethod.GET)
     public ResponseEntity<String> getAnonymous() {
+
+
         return ResponseEntity.ok("Hello Anonymous");
     }
 
     @RolesAllowed("user")
     @RequestMapping(value = "/user", method = RequestMethod.GET)
-    public ResponseEntity<String> getUser(@RequestHeader String Authorization) {
-        log.info(Authorization);
+    public ResponseEntity<String> getUser(@RequestHeader String Authorization) throws JsonProcessingException {
 
-        return ResponseEntity.ok("Hello User");
+        return ResponseEntity.ok("Hello User " + jwtDecoder.retrieveUsername(Authorization));
     }
+
 
     @RolesAllowed("admin")
     @RequestMapping(value = "/admin", method = RequestMethod.GET)
